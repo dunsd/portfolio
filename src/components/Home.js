fffd97b556c162e85160d56aa74682aed4aa26f4
dunsd <dthomasparker@gmail.com>
2023-05-08 20:33:53 +0100

    Add links component and create styles.scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
 const Home = () => {
-
-    return (
-        <div>
-            <h1>Hello, my name is Daniel.</h1>
-            <p>
-                I am an aspiring web developer currently working as an Electrical Design Engineer.
-            </p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="introCard">
+        <h1 className="introHeader">Hello, my name is Daniel.</h1>
+        <p className="introPara">
+          I am an aspiring Software Engineer currently working as an Electrical
+          Design Engineer.
+        </p>
+        <Links />
+      </div>
+    </div>
+  );
+};
 export default Home;
---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Links = () => {
+    return (
+        <div className="links">
+            <a href="https://github.com/dunsd">Github</a>
+            <a href="https://linkedin.com/in/daniel-parker-46abab173">
+                LinkedIn
+            </a>
+        </div>
+    )
+}
+export default Links;
