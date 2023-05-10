6c3ae021b3398cc3cb326c14918b575072196552
dunsd <dthomasparker@gmail.com>
2023-05-10 23:07:54 +0100

    Add border styling and additional skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,11 +3,21 @@ import React from "react";
 const Skills = () => {
     return (
         <ul className="skills">
-            <li><h3>Skills:</h3></li>
-            <li>Javascript</li>
-            <li>React</li>
-            <li>HTML5</li>
-            <li>CSS - Flexbox, CSS Grid</li>
+            <li><h3 className="skillsHeader">Skills:</h3></li>
+
+            <ul> <p className="focusHeader">Current Focus:</p>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>HTML5</li>
+                <li>CSS - Flexbox, CSS Grid</li>
+            </ul>
+            <ul>
+                <p className="focusHeader">Experience with:</p>
+                <li>C++</li>
+                <li>C</li>
+                <li>Embedded C</li>
+            </ul>
+            
         </ul>
     )
 }
