9b5652152782487a3aac8c8b8ac46bec2c55b944
dunsd <dthomasparker@gmail.com>
2023-05-11 18:00:06 +0100

    Add details to prj overview

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,19 @@
 import React from "react";
+import realmLibrary from "../images/realm-reviews-library.png";
 
 const Projects = () => {
 
     return (
-        <div>
-            Projects here
+        <div className="prjContent">
+            <div className="introPrjCard">
+                <h1>Project Overview</h1>
+                <div>
+                    <h2>Realm Reviews</h2>
+                    <p>A CRUD website based on books.</p>
+                    <p>Technologies: React, Node, Express, MongoDB</p>
+                    <img src={realmLibrary} alt="Realm Reviews website"></img>
+                </div>
+            </div>
         </div>
     )
 }
